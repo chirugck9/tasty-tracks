f87f5958b1d1966069e217b5e9268d04e110d5ea
Chinmai Kumar Gadakary <chiru.gadakary@gmail.com>
2024-04-16 22:48:48 +0530

    orders crud apis

--- server/src/controllers/orders.js
+++ server/src/controllers/orders.js
@@ -31,20 +31,21 @@ const createOrder = async (req, res) => {
 	try {
 		const result = await db.query(orderQuery);
 		const orderId = result.rows[0].order_id;
-		for (const item of order_items) {
-			const { menu_item_id, quantity, price, special_instructions } = item;
-			const itemsQuery = {
-				text: "INSERT INTO order_items (order_id,menu_item_id,quantity,price,special_instructions) VALUES($1,$2,$3,$4,$5) RETURNING *",
-				values: [
-					orderId,
-					item.menu_item_id,
-					item.quantity,
-					item.price,
-					item.special_instructions,
-				],
-			};
-			const itemResult = await db.query(itemsQuery);
-		}
+		// for (const item of order_items) {
+		// 	const { menu_item_id, quantity, price, special_instructions } = item;
+		// 	const itemsQuery = {
+		// 		text: "INSERT INTO order_items (order_id,menu_item_id,quantity,price,special_instructions) VALUES($1,$2,$3,$4,$5) RETURNING *",
+		// 		values: [orderId, menu_item_id, quantity, price, special_instructions],
+		// 	};
+		// 	const itemResult = await db.query(itemsQuery);
+		// }
+		const { menu_item_id, quantity, price, special_instructions } = req.body;
+		const itemsQuery = {
+			text: "INSERT INTO order_items (order_id,menu_item_id,quantity,price,special_instructions) VALUES($1,$2,$3,$4,$5) RETURNING *",
+			values: [orderId, menu_item_id, quantity, price, special_instructions],
+		};
+		const itemResult = await db.query(itemsQuery);
+
 		return res.status(201).json({
 			success: true,
 			message: "Order created succesfully",
@@ -86,7 +87,106 @@ const getAllOrders = async (req, res) => {
 	}
 };
 
+//get order by id
+const getOrderByid = async (req, res) => {
+	try {
+		const query = {
+			text: "SELECT * FROM orders WHERE order_id = $1",
+			values: [req.params.order_id],
+		};
+		const result = await db.query(query);
+		if (result.rows[0]) {
+			return res.status(200).json({
+				success: true,
+				message: "Order retrived succesfully",
+				data: result.rows[0],
+			});
+		} else {
+			throw new Error("Order not found");
+		}
+	} catch (error) {
+		console.error(error);
+		throw new Error("An error occured while fetching order by id");
+	}
+};
+
+//update order by id
+const updateOrderById = async (req, res) => {
+	const {
+		order_id,
+		customer_id,
+		restaurant_id,
+		delivery_person_id,
+		delivery_address,
+		city,
+		state,
+		zipcode,
+		country,
+		total_price,
+		order_items,
+	} = req.body;
+	const orderId = req.params.order_id;
+	try {
+		const query = {
+			text: "UPDATE orders SET customer_id = $1 , restaurant_id= $2 , delivery_person_id = $3 ,delivery_address = $4 , city = $5, state = $6,zipcode = $7 , country = $8 ,total_price=$9,order_items=$10 WHERE order_id = $11 RETURNING *",
+			values: [
+				order_id,
+				customer_id,
+				restaurant_id,
+				delivery_person_id,
+				delivery_address,
+				city,
+				state,
+				zipcode,
+				country,
+				total_price,
+				order_items,
+				orderId,
+			],
+		};
+		const result = await db.query(query);
+
+		if (result.rowCount === 1) {
+			return res.status(200).json({
+				success: true,
+				message: "Order updated succesfully",
+				data: result.rows[0],
+			});
+		} else {
+			throw new Error("Order not found");
+		}
+	} catch (error) {
+		console.error(error);
+		throw new Error("An error has occured while updating the order");
+	}
+};
+
+//delete order
+const deleteOrderById = async (req, res) => {
+	try {
+		const query = {
+			text: "DELETE FROM orders WHERE order_id = $1",
+			values: [req.params.order_id],
+		};
+		const result = await db.query(query);
+		if (result.rowCount === 1) {
+			return res.status(200).json({
+				success: true,
+				message: "order deleted succesfully",
+				data: result.rows[0],
+			});
+		} else {
+			throw new Error("order not found");
+		}
+	} catch (error) {
+		console.error(error);
+		throw new Error("An error occured while deleting the order");
+	}
+};
 module.exports = {
 	createOrder,
 	getAllOrders,
+	getOrderByid,
+	updateOrderById,
+	deleteOrderById,
 };
--- server/src/routes/orders.js
+++ server/src/routes/orders.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
-const { createOrder, getAllOrders } = require("../controllers/orders");
+const {
+	createOrder,
+	getAllOrders,
+	getOrderByid,
+	updateOrderById,
+	deleteOrderById,
+} = require("../controllers/orders");
 
 const router = Router();
 
 router.post("/orders", createOrder);
 router.get("/orders/:page_id/:limit", getAllOrders);
+router.get("/orders/:order_id", getOrderByid);
+router.put("/orders/:order_id", updateOrderById);
+router.delete("/orders/:order_id", deleteOrderById);
 
 module.exports = router;
