d7a6e19dedf4608028e2aec0cf1561813179f60e
Chinmai Kumar Gadakary <chiru.gadakary@gmail.com>
2024-04-22 23:37:14 +0530

    secret updated

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -1,9 +1,17 @@
 const { config } = require("dotenv");
 config();
 
+// module.exports = {
+// 	PORT: process.env.PORT,
+// 	SERVER_URL: process.env.SERVER_URL,
+// 	CLIENT_URL: process.env.CLIENT_URL,
+// 	SECRET: process.env.SECRET,
+// };
+
 module.exports = {
-	PORT: process.env.PORT,
-	SERVER_URL: process.env.SERVER_URL,
-	CLIENT_URL: process.env.CLIENT_URL,
-	SECRET: process.env.SECRET,
+	PORT: 8000,
+	SECRET: "QWERTY123456UIOP789",
+
+	CLIENT_URL: "http://localhost:3000",
+	SERVER_URL: "http://localhost:8000",
 };
