7e54f97fa23d951c2eef06bc415e1bbd93def06e
Chinmai Kumar Gadakary <chiru.gadakary@gmail.com>
2024-04-23 11:33:44 +0530

    PORT updated

--- server/src/index.js
+++ server/src/index.js
@@ -38,7 +38,7 @@ app.use("/api", userAuth, ratings);
 const appStart = () => {
 	try {
 		app.listen(PORT, () => {
-			console.log(`The app is running at ${PORT}`);
+			console.log(`The app is running at http://localhost:${PORT}`);
 		});
 	} catch (error) {
 		console.log(`Error:${error.message}`);
